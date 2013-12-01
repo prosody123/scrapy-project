bee4f52f8f51e0961c4b8f9e283b7caeedf39578
Parveen <tparvee@MojoJojo.local>
2013-12-01 20:50:19 +0530

    Adding project_v1

---
+++ project_v1/project_v1/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for project_v1 project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'project_v1'
+
+SPIDER_MODULES = ['project_v1.spiders']
+NEWSPIDER_MODULE = 'project_v1.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'project_v1 (+http://www.yourdomain.com)'
---
+++ project_v1/project_v1/spiders/dmoz_spider.py
@@ -0,0 +1,27 @@
+
+from scrapy.spider import BaseSpider
+from scrapy.selector import Selector
+from project_v1.items import DmozItem 
+class DmozSpider(BaseSpider):
+	name = "dmoz"
+	allowed_domains = ["dmoz.org"]
+	start_urls = ["http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"]
+	
+	def parse(self, response):
+		filename = response.url.split("/")[-2]
+		open(filename, 'wb').write(response.body)
+		#print "url is: " + response.url
+		#print "Filename is: " + filename
+		sel = Selector(response)
+		sites = sel.xpath('//ul/li')
+		items = []
+		for site in sites:
+			#print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+			#print site
+			item = DmozItem()
+			item['title'] = site.xpath('a/text()').extract()
+			item['link'] = site.xpath('a/@href').extract()
+			item['desc'] = site.xpath('text()').extract()
+			items.append(item)
+		return items
